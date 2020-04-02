08874edb69906dde6e73b203c3271ea719b40a09
ruwendra <ruwen@cinergix.com>
2020-04-02 11:24:15 +0530

    added more comments to each variable

--- packages/npm2gpr/examples/example-1.js
+++ packages/npm2gpr/examples/example-1.js
@@ -1,5 +1,9 @@
+// require index.js mentioned within package.json
 const npm2gpr = require("../");
 
+/**
+ *  function to call getModuleNames and return results on console.log
+ */
 async function main(){
   const names = await npm2gpr.getModuleNames("creately");
   console.log(names);
--- packages/npm2gpr/src/index.js
+++ packages/npm2gpr/src/index.js
@@ -1,3 +1,4 @@
+// additional library used to fetch data for a given url
 const fetch = require("node-fetch");
 
 /**
@@ -13,8 +14,11 @@ function getSearchUrl(org, from = 0) {
  * @param {int} from npm page count
  */
 async function getSearchPage(org, from = 0) {
+  // return response of given org name and starting object count
   const response = await fetch(getSearchUrl(org, from));
+  // return result in json format
   const json = await response.json();
+  // result array to concat objects:package.name specifically searched by "@creately/"
   const results = [];
   for (var i = 0; i < json.objects.length; i++) {
     const name = json.objects[i].package.name;
@@ -33,10 +37,14 @@ async function getSearchPage(org, from = 0) {
  * @param {String} org npm organization name
  */ 
 async function getModuleNames(org) {
+  // array to store set of elements pushed for each page by url
   const results = [];
+  // initial object count from the getSearchPage function
   let from = 0;
   while (true) {
+    // results of objects returned in json format
     const pageResults = await getSearchPage(org, from);
+    // exit while loop when results returned from getSearchPage function returns zero
     if (pageResults.results.length === 0) {
       break;
     }
@@ -46,6 +54,9 @@ async function getModuleNames(org) {
   return results;
 }
 
+/**
+ * export function getModuleNames to get use globally
+ */
 module.exports = {
   getModuleNames,
 };
