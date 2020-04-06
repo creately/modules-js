6e77c734ce8c0163dd4c4289093fda01c98ee563
Muhammed Thanish <mnmtanish@gmail.com>
2020-04-06 15:58:48 +0530

    Revert "Use rollup to bundle styles with js"
    This reverts commit 241c926675d9364f0a23c8bd7a7fd0c5785cb9af.

--- packages/elements/src/components/button/button.tsx
+++ packages/elements/src/components/button/button.tsx
@@ -1,11 +1,9 @@
-import "./button.scss";
 import React from "react";
 
 export interface ButtonProps {
-  primary?: boolean;
+  // label?: string;
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const className = props?.primary ? "button primary" : "button";
-  return <button className={className}>{props.children}</button>;
+  return <button>{props.children}</button>;
 };
