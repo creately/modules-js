4b73d34771f87e45286843de1416054fb8de17eb
Vinoch <vinoch@Vinoch-Selvarathinam.local>
2020-04-27 09:34:41 +0530

    ran lint

--- packages/clipboard/src/__tests__/index.spec.ts
+++ packages/clipboard/src/__tests__/index.spec.ts
@@ -1,7 +1,7 @@
 import { Clipboard } from '../';
 import * as clipboardpoly from 'clipboard-polyfill';
 
-describe( 'Clipboard', () => {
+describe('Clipboard', () => {
   let clipboard: any;
   beforeEach(() => {
     clipboard = new Clipboard();
@@ -9,69 +9,69 @@ describe( 'Clipboard', () => {
 
   describe('copy', () => {
     it('should copy data to local clipboard', () => {
-      spyOn( clipboardpoly, 'writeText' ).and.returnValue( Promise.reject("test error"));
-      spyOn( clipboard, 'storeToLocalClipboard' );
-      clipboard.copy( 'success' ).subscribe(() => {
-        expect( clipboardpoly.writeText ).toHaveBeenCalledWith( 'success' );
-        expect( clipboard.storeToLocalClipboard ).toHaveBeenCalledWith( 'success' );
+      spyOn(clipboardpoly, 'writeText').and.returnValue(Promise.reject('test error'));
+      spyOn(clipboard, 'storeToLocalClipboard');
+      clipboard.copy('success').subscribe(() => {
+        expect(clipboardpoly.writeText).toHaveBeenCalledWith('success');
+        expect(clipboard.storeToLocalClipboard).toHaveBeenCalledWith('success');
       });
     });
     it('should copy data to system clipboard', () => {
-      spyOn( clipboardpoly, 'writeText' ).and.returnValue( Promise.resolve("test success"));
-      spyOn( clipboard, 'storeToLocalClipboard' );
-      clipboard.copy( 'success' ).subscribe(() => {
-        expect( clipboardpoly.writeText ).toHaveBeenCalledWith( 'success' );
-        expect( clipboard.storeToLocalClipboard ).not.toHaveBeenCalled();
+      spyOn(clipboardpoly, 'writeText').and.returnValue(Promise.resolve('test success'));
+      spyOn(clipboard, 'storeToLocalClipboard');
+      clipboard.copy('success').subscribe(() => {
+        expect(clipboardpoly.writeText).toHaveBeenCalledWith('success');
+        expect(clipboard.storeToLocalClipboard).not.toHaveBeenCalled();
       });
     });
   });
 
   describe('paste', () => {
     it('should return data from system clipboard', () => {
-      spyOn( clipboardpoly, 'readText' ).and.returnValue( Promise.resolve("test success"));
-      spyOn( clipboard, 'retriveLocalClipboardData' );
-      clipboard.paste().subscribe(( val: any ) => {
-        expect( val ).toEqual( 'test success' );
-        expect( clipboardpoly.readText ).toHaveBeenCalled();
-        expect( clipboard.retriveLocalClipboardData ).not.toHaveBeenCalled();
+      spyOn(clipboardpoly, 'readText').and.returnValue(Promise.resolve('test success'));
+      spyOn(clipboard, 'retriveLocalClipboardData');
+      clipboard.paste().subscribe((val: any) => {
+        expect(val).toEqual('test success');
+        expect(clipboardpoly.readText).toHaveBeenCalled();
+        expect(clipboard.retriveLocalClipboardData).not.toHaveBeenCalled();
       });
     });
     it('should return data from local clipboard', () => {
-      spyOn( clipboardpoly, 'readText' ).and.returnValue( Promise.resolve( '' ));
-      spyOn( clipboard, 'retriveLocalClipboardData' );
+      spyOn(clipboardpoly, 'readText').and.returnValue(Promise.resolve(''));
+      spyOn(clipboard, 'retriveLocalClipboardData');
       clipboard.paste().subscribe(() => {
-        expect( clipboardpoly.readText ).toHaveBeenCalled();
-        expect( clipboard.retriveLocalClipboardData ).toHaveBeenCalled();
+        expect(clipboardpoly.readText).toHaveBeenCalled();
+        expect(clipboard.retriveLocalClipboardData).toHaveBeenCalled();
       });
     });
     it('should return data from local clipboard on system clipboard error', () => {
-      spyOn( clipboardpoly, 'readText' ).and.returnValue( Promise.reject( 'system error' ));
-      spyOn( clipboard, 'retriveLocalClipboardData' );
+      spyOn(clipboardpoly, 'readText').and.returnValue(Promise.reject('system error'));
+      spyOn(clipboard, 'retriveLocalClipboardData');
       clipboard.paste().subscribe(() => {
-        expect( clipboardpoly.readText ).toHaveBeenCalled();
-        expect( clipboard.retriveLocalClipboardData ).toHaveBeenCalled();
+        expect(clipboardpoly.readText).toHaveBeenCalled();
+        expect(clipboard.retriveLocalClipboardData).toHaveBeenCalled();
       });
     });
   });
 
   describe('storeToLocalClipboard', () => {
     it('should not clear data from local storage if there are any', () => {
-      spyOn( clipboard, 'retriveLocalClipboardData' ).and.returnValue( undefined );
-      spyOn( clipboard, 'removeLocalClipboardData' );
+      spyOn(clipboard, 'retriveLocalClipboardData').and.returnValue(undefined);
+      spyOn(clipboard, 'removeLocalClipboardData');
       jest.spyOn(window.localStorage.__proto__, 'setItem');
       window.localStorage.__proto__.setItem = jest.fn();
-      clipboard.storeToLocalClipboard( 'success' );
-      expect( clipboard.removeLocalClipboardData ).not.toHaveBeenCalled();
-      expect( localStorage.setItem ).toHaveBeenCalledWith( 'clipboardData', 'success' );
+      clipboard.storeToLocalClipboard('success');
+      expect(clipboard.removeLocalClipboardData).not.toHaveBeenCalled();
+      expect(localStorage.setItem).toHaveBeenCalledWith('clipboardData', 'success');
     });
     it('should clear data from local storage if there are any', () => {
-      spyOn( clipboard, 'retriveLocalClipboardData' ).and.returnValue( 'something' );
-      spyOn( clipboard, 'removeLocalClipboardData' );
+      spyOn(clipboard, 'retriveLocalClipboardData').and.returnValue('something');
+      spyOn(clipboard, 'removeLocalClipboardData');
       jest.spyOn(window.localStorage.__proto__, 'setItem');
       window.localStorage.__proto__.setItem = jest.fn();
-      clipboard.storeToLocalClipboard( 'success' );
-      expect( clipboard.removeLocalClipboardData ).toHaveBeenCalled();
-      expect( localStorage.setItem ).toHaveBeenCalledWith( 'clipboardData', 'success' );
+      clipboard.storeToLocalClipboard('success');
+      expect(clipboard.removeLocalClipboardData).toHaveBeenCalled();
+      expect(localStorage.setItem).toHaveBeenCalledWith('clipboardData', 'success');
     });
   });
 
@@ -80,24 +80,24 @@ describe( 'Clipboard', () => {
       jest.spyOn(window.localStorage.__proto__, 'getItem');
       window.localStorage.__proto__.getItem = jest.fn();
       clipboard.retriveLocalClipboardData();
-      expect( localStorage.getItem ).toHaveBeenCalledWith( 'clipboardData' );
+      expect(localStorage.getItem).toHaveBeenCalledWith('clipboardData');
     });
   });
 
   describe('removeLocalClipboardData', () => {
     it('should remove data from local storage', () => {
-      spyOn( clipboard, 'retriveLocalClipboardData' ).and.returnValue( 'something' );
+      spyOn(clipboard, 'retriveLocalClipboardData').and.returnValue('something');
       jest.spyOn(window.localStorage.__proto__, 'removeItem');
       window.localStorage.__proto__.removeItem = jest.fn();
       clipboard.removeLocalClipboardData();
-      expect( localStorage.removeItem ).toHaveBeenCalledWith( 'clipboardData' );
+      expect(localStorage.removeItem).toHaveBeenCalledWith('clipboardData');
     });
     it('should not remove data from local storage', () => {
-      spyOn( clipboard, 'retriveLocalClipboardData' ).and.returnValue( undefined );
+      spyOn(clipboard, 'retriveLocalClipboardData').and.returnValue(undefined);
       jest.spyOn(window.localStorage.__proto__, 'removeItem');
       window.localStorage.__proto__.removeItem = jest.fn();
       clipboard.removeLocalClipboardData();
-      expect( localStorage.removeItem ).not.toHaveBeenCalled();
+      expect(localStorage.removeItem).not.toHaveBeenCalled();
     });
   });
 });
