cb0335e45399055dc39474acc04893ca07b6891d
chandika <chandika@gmail.com>
2020-05-29 07:15:02 +1000

    added plugin command

---
+++ packages/client-sdk-events/src/plugin.ts
@@ -0,0 +1,17 @@
+export enum PluginCommands {
+    
+    /**
+     * Sends the init message to the plugin after it's loaded.
+     * Also sets an identifier for the plugins to communicate via postMessage.
+     * Done only at initialization.
+     */
+    pluginInit = 'creately:plugin:init',
+
+    /**
+     * Tells the plugin that its being unloaded from the host env
+     */
+    pluginUnload = 'creately:plugin:unload',
+
+
+}
+  
